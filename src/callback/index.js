dbee4a61afce442f8d92dda628fc47a959a4f847
jorge zubieta <zubietajor@gmail.com>
2021-06-15 06:07:22 -0300

    setTimeout added

--- src/callback/index.js
+++ src/callback/index.js
@@ -7,3 +7,21 @@ function calc(num1, num2, callback) {
 }
 
 console.log(calc(2,2, sum));
+
+// 1ra funcion que se ejecuta
+function date(callback) {
+    console.log(new Date);
+
+    setTimeout(function() {
+        let date = new Date;
+        callback(date);
+    }, 3000)
+
+}
+
+// 2nd funcion que imprime esos valors
+function printDate(dateNow) {
+    console.log(dateNow);
+}
+
+date(printDate);
