95ec4dc2492fe0b06102746dfcd570b066fdc617
jorge zubieta <zubietajor@gmail.com>
2021-06-19 13:12:38 -0300

    promise 2 with seTimout added

--- src/promise/index.js
+++ src/promise/index.js
@@ -19,3 +19,29 @@ somethingWillHappen ()
     .catch(err => console.error(err));
 // procedemos a crear nuestro scrip en package.json oara poder correr este codigo que acabamos de crear.
 
+
+
+// -------Ejemplo 2------- 
+// Promise. Nueva promesa para ver la parte del tiempo!
+// tener en cuenta que al erro typo "P" para escribir Promise y no el error "promise"!
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => {
+                resolve('Is true, forever i will love you guys, Daddy!');                
+            }, 5000);
+        } else {
+            const error = new Error('WhooooooOps... Error!');
+            reject(error);
+        }
+    });    
+}
+// Procedemos a ejecutar la nerw promeise!
+somethingWillHappen2()
+    .then(Response => console.log(Response))
+    // puedo realizar encadenamietos de acciones por ejemplo, despues del primer then!
+    .then(() => console.log('----------------------------------'))
+    .then(() => console.log('seTimout : Solucion en 5 segundos!'))
+    .then(() => console.log('----------------------------------'))
+    .catch(err => console.error(err))
+    // mostrara los detalles la ruta de lo que sucedio en el error
